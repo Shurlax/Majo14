04dcdc08a4ceaa19606cceecbedd497d00d99f3b
shurlax <shuouma06@gmail.com>
2025-02-14 00:46:21 -0600

    Deploy to GitHub Pages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./app.css";
+import Header from "./components/Header";
+import Historia from "./components/Historia";
+import Galeria from "./components/Galeria";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Historia />
+      <Galeria />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+          return (
+            <section className="flex flex-col items-center text-center p-8">
+            <h1 className="text-5xl font-bold text-pink-600 mb-4">😚 💖 Nuestra Historia Recién Comienza 💖😚</h1>
+            <p className="text-xl text-gray-700">Un viaje lleno de amor y momentos inolvidables.</p>
+            </section>
+          );
+        };
+        
+        export default Hero;
