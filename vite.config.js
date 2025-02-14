dc80f5b3322fd41fd9c7276626cebbd7c9e9435b
shurlax <shuouma06@gmail.com>
2025-02-14 11:22:59 -0600

    Correcion cambio nombre

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Majo14/',  // Asegúrate de que este sea el subdirectorio correcto en GitHub Pages
+  base: '/Majo14/',
 })
