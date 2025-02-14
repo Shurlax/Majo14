3255115db4fd1adfc450317d272fc0769a9f0438
shurlax <shuouma06@gmail.com>
2025-02-14 00:09:46 -0600

    PAGES

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Majo14/',  // Asegúrate de que este sea el subdirectorio correcto en GitHub Pages
 })
